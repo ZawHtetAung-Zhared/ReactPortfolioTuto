dfbb94c1ea60f6ad4358f1f9143e2fd7de7f77b3
ZawHtetAung-Zhared <zawhtetaung.zhared@gmail.com>
2022-05-08 21:15:54 +0630

    contact and footer added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Portfolio from "./Component/Portfolio/Portfolio";
 import Resume from './Component/Resume/Resume';
 import Testimonial from './Component/Testimonial/Testimonial';
 import Blog from './Component/Blog/Blog';
+import Contact from './Component/Contact/Contact';
+import Footer from './Component/Footer';
 
 const App = () => {
   return (
@@ -18,6 +20,8 @@ const App = () => {
     <Resume />
     <Testimonial />
     <Blog />
+    <Contact />
+    <Footer />
     </>
   )
 }
